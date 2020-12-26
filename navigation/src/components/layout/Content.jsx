0c553bec0151d95061766991dc685a9a8c6c2d3f
francisco ximenes <fximenessilva@gmail.com>
2020-12-26 14:24:10 +0000

    added full navigation

--- navigation/src/components/layout/Content.jsx
+++ navigation/src/components/layout/Content.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Content.css';
+import {Switch, Route} from 'react-router-dom'
+import About from '../../views/examples/About'
+import Home from '../../views/examples/Home'
+import Param from '../../views/examples/Param'
 
 export default function Content(props) {
     return(
         <main className="Content">
-            Componente Content
+            <Switch>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/param/:id">
+                    <Param />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
         </main>
     )
 }
