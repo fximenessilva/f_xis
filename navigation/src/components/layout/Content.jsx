b83faa7e53d080086685aebd2bc06b544e0e1da3
francisco ximenes <fximenessilva@gmail.com>
2020-12-26 12:37:30 +0000

    added layout

---
+++ navigation/src/components/layout/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Content.css';
+
+export default function Content(props) {
+    return(
+        <main className="Content">
+            Componente Content
+        </main>
+    )
+}
